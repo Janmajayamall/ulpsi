b4a46b5c525edb5062bb5d22e7d4db952376c549
Janmajaya Mall <janmajayamall@protonmail.com>
2023-08-29 12:59:38 +0530

    add server crate

---
+++ server/src/main.rs
@@ -0,0 +1,15 @@
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        // The second item contains the IP and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
+        // process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {}
